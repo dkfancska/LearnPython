4fe0b9a53169f14f68ec3091af455aaf54a37e21
Kirill Dolinin <qwertyfreak@mail.ru>
2021-01-17 21:31:14 +0300

    Create second homework from course

---
+++ task2.py
@@ -0,0 +1,6 @@
+number = input()  # не меньше 1 и не больше 9
+print('   _~_    ' * int(number))
+print('  (o o)   ' * int(number))
+print(' /  V  \\  ' * int(number))
+print('/(  _  )\\ ' * int(number))
+print('  ^^ ^^   ' * int(number))
