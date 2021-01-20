dfcc77652bb90f5cdf2bf3655bc5d21cff8e707c
Kirill Dolinin <qwertyfreak@mail.ru>
2021-01-20 23:13:31 +0300

    fixing E501 line too long

---
+++ week 1/task17.py
@@ -0,0 +1,13 @@
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+total1 = (h1 * 60 * 60) + (m1 * 60) + s1
+
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+total2 = (h2 * 60 * 60) + (m2 * 60) + s2
+
+result = (total2 - total1)
+
+print(result)
